57977b22a95e5b175aa8daa4167b28c11494202d
Emre Gürler <emregurler93@gmail.com>
2022-03-12 16:08:55 +0300

    four splitters implemented as designed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import React from 'react';
 import './App.scss';
 
+import React from 'react';
+import Split from 'react-split';
+import { Header } from 'components';
+
+function C1() {
+  return <div class="comp">Component 1 {Date.now()}</div>;
+}
+function C2() {
+  return <div class="comp">Component 2 {Date.now()}</div>;
+}
+
+function UpperHorizontal() {
+  return (
+    <div>
+      <Split className="horizontal-splitter" direction="horizontal" sizes={[50, 50]}>
+        <C1 />
+        <C2 />
+      </Split>
+    </div>
+  );
+}
+
+function BottomHorizontal() {
+  return (
+    <div>
+      <Split className="horizontal-splitter" direction="horizontal" sizes={[50, 50]}>
+        <C1 />
+        <C2 />
+      </Split>
+    </div>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="content">
+        <Split className="vertical-splitter" direction="vertical" sizes={[50, 50]}>
+          <UpperHorizontal />
+          <BottomHorizontal />
+        </Split>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import style from './Header.module.scss';
+
+import React from 'react';
+
+const Header = () => {
+  return <div className={style.header}>Header</div>;
+};
+
+export default Header;
